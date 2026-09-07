75ca856ae65fbf0a7369b499e045d10fb875d644
Casey <108437478+casey-shea05@users.noreply.github.com>
2026-09-07 15:06:31 +0100

    feat(sumAll): complete exercises and check that all tests pass

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
 
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    return "ERROR";
+  }
+
+  const list = [];
+  const start = Math.min(a, b);
+  const end = Math.max(a, b);
+
+  for (let i = start; i <= end; i++) {
+    list.push(i);
+  }
+
+  return list.reduce((x, y) => x + y);
 };
 
 // Do not edit below this line
