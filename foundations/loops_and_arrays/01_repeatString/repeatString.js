80bca2d7c4ddbecf36a722b014f37cedf387d2be
Casey <108437478+casey-shea05@users.noreply.github.com>
2026-09-07 12:37:28 +0100

    feat(repeatString): complete exercises and check all tests pass

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (word, repeats) {
+  if (repeats < 0) {
+    return "ERROR";
+  }
 
+  const repeatedString = [];
+
+  for (let i = 0; i < repeats; i++) {
+    repeatedString[i] = word;
+  }
+
+  return repeatedString.join("");
 };
 
 // Do not edit below this line
