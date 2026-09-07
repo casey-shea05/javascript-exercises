9b6ab1aacfce8bd06c1070b7f3914042c0d5e7cf
Casey <108437478+casey-shea05@users.noreply.github.com>
2026-09-07 14:35:09 +0100

    refactor(removeFromArray): better solution using filter

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,14 +1,5 @@
 const removeFromArray = function (array, ...Values) {
-  for (const value of Values) {
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] === value) {
-        array.splice(i, 1);
-        i--;
-      }
-    }
-  }
-
-  return array;
+  return array.filter (value => !Values.includes(value));
 };
 
 // Do not edit below this line
