020712a10dd0446fd534232b3ef9c9cbfc4aa9eb
Casey <108437478+casey-shea05@users.noreply.github.com>
2026-09-07 14:20:33 +0100

    feat(removeFromArray): complete exercise and check all tests pass

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...Values) {
+  for (const value of Values) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === value) {
+        array.splice(i, 1);
+        i--;
+      }
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
