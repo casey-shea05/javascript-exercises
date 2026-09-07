3862c58715f4178d36d0b577a391acc167b387a1
Casey <108437478+casey-shea05@users.noreply.github.com>
2026-09-07 12:58:39 +0100

    refactor(reverseString): simpler solution

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function (input) {
-  let reversedInput = "";
-  for (let i = input.length - 1; i >= 0; i--) {
-    reversedInput += input[i];
-  }
-
-  return reversedInput;
+  return input.split("").reverse().join("");
 };
 
 // Do not edit below this line
