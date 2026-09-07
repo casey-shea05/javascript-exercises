5b48de106c21a8a778d1f8999f615a367263d6de
Casey <108437478+casey-shea05@users.noreply.github.com>
2026-09-07 12:56:00 +0100

    feat(reverseString): complete exercises and ensure all tests pass

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (input) {
+  let reversedInput = "";
+  for (let i = input.length - 1; i >= 0; i--) {
+    reversedInput += input[i];
+  }
 
+  return reversedInput;
 };
 
 // Do not edit below this line
